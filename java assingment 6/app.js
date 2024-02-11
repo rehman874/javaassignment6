b231a77562f2fe344550e86a26d55e450077fb91
Abdul Rehman Khan <155987786+rehman874@users.noreply.github.com>
2024-02-11 15:35:20 +0500

    Add files via upload

---
+++ java assingment 6/app.js
@@ -0,0 +1,9 @@
+document.getElementById("button").addEventListener("click", function() {
+    alert('Button Clicked!');
+});
+ 
+div.addEventListener("mouseover",() => div.style = "background-color:pink");
+div.addEventListener("mouseout",() => div.style = "");
+
+
+
